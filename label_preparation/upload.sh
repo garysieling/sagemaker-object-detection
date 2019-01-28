601e4a500e37bdc20ddeeb7d11bd53e8660f95c5
Gary Sieling <gary@garysieling.com>
2019-01-27 19:29:32 -0500

    add upload script

---
+++ label_preparation/upload.sh
@@ -0,0 +1,5 @@
+aws s3 sync ./aws_labels s3://sieling.household/train_annotation
+aws s3 sync ./validation_labels s3://sieling.household/validation_annotation
+aws s3 sync ./validation s3://sieling.household/validation
+aws s3 sync ./images s3://sieling.household/train/images
+
