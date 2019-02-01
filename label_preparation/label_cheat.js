0598d6350f47bb5bc85787232e33db1ee1015f8c
Gary Sieling <gary@garysieling.com>
2019-01-31 22:15:14 -0500

    initial label generation from video clips

--- label_preparation/label_cheat.js
+++ label_preparation/label_cheat.js
@@ -2,5 +2,32 @@ const fs = require('fs');
 const type = process.argv[2]
 
 const data = {};
+let text = '';
 
-fs.writeFileSync('validation_' + type + '.json', JSON.stringify(data, null, 2));
+const files = fs.readdirSync('temp/');
+files.map(
+  (file) => {
+    text += JSON.stringify(
+      {
+        "content": "images/" + file,
+        "annotation":[
+          {
+            "shape":"rectangle",
+            "points":[[0,0],[1,0],[0,1],[1,1]],
+            "notes":"",
+            "imageWidth":500,"imageHeight":500
+          }
+        ],
+        "extras":null,
+        "metadata":{
+          "first_done_at":0,
+          "last_updated_at":0,
+          "sec_taken":0,
+          "last_updated_by":"",
+          "status":"done","evaluation":"NONE"
+        }
+      }) + "\n";
+  }
+);
+
+fs.writeFileSync('validation_' + type + '.json', text);
